dfbbcd5bb3b4dba99549de5794a26609f0c8482d
Risto Saarelma <risto.saarelma@iki.fi>
2021-12-04 07:45:16 +0200

    Eprint

--- py/prelude.py
+++ py/prelude.py
@@ -3,6 +3,10 @@ import sys
 from typing import IO, Tuple
 
 
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
 def read(parse=str, sep: str = '\n', f: IO = sys.stdin):
     """Read homogeneous `sep` separated input from given file."""
     return [parse(elt) for elt in f.read().rstrip().split(sep)]
