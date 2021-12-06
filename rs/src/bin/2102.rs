a20241f5ce5d4e75e82518a20609f5a1d3075249
Risto Saarelma <risto.saarelma@iki.fi>
2021-12-06 09:41:03 +0200

    Day 2021.02 (rs) touchup

--- rs/src/bin/2102.rs
+++ rs/src/bin/2102.rs
@@ -1,4 +1,4 @@
-use std::io::{stdin, BufRead};
+use aoc::prelude::*;
 
 fn f1((x, y): (i32, i32), &(ref cmd, a): &(String, i32)) -> (i32, i32) {
     let (dx, dy) = match cmd.as_ref() {
@@ -21,13 +21,10 @@ fn f2((x, y, z): (i32, i32, i32), &(ref cmd, a): &(String, i32)) -> (i32, i32, i
 }
 
 fn main() {
-    let moves: Vec<(String, i32)> = stdin()
-        .lock()
-        .lines()
-        .map(|s| {
-            let s = s.unwrap();
-            let elts: Vec<&str> = s.split(' ').collect();
-            (elts[0].to_string(), elts[1].parse::<i32>().unwrap())
+    let moves: Vec<(String, i32)> = stdin_lines()
+        .map(|s| match s.split(' ').collect::<Vec<&str>>().as_slice() {
+            [dir, n] => (dir.to_string(), n.parse::<i32>().unwrap()),
+            _ => panic!(),
         })
         .collect();
 
