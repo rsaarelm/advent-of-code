0056cce12fa6377c7bd4380b446f33e89db18d31
Risto Saarelma <risto.saarelma@iki.fi>
2021-12-06 10:07:40 +0200

    Formatting

--- py/a2101.py
+++ py/a2101.py
@@ -1,6 +1,6 @@
-from prelude import *
+from prelude import read
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     nums = read(int)
 
     n = 0
@@ -9,7 +9,7 @@ if __name__ == '__main__':
             n += 1
     print(n)
 
-    nums2 = [nums[i] + nums[i-1] + nums[i-2] for i in range(2, len(nums))]
+    nums2 = [nums[i] + nums[i - 1] + nums[i - 2] for i in range(2, len(nums))]
     n = 0
     for i in range(1, len(nums2)):
         if nums2[i] > nums2[i - 1]:
