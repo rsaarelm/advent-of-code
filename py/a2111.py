48bac4de5dbbca69ded736de9316cb0f80874593
Risto Saarelma <risto.saarelma@iki.fi>
2021-12-11 07:43:02 +0200

    Object-orientate

--- py/a2111.py
+++ py/a2111.py
@@ -1,67 +1,71 @@
 from copy import deepcopy
 from prelude import read
 
-if __name__ == '__main__':
+
+class Squids:
+    def __init__(self, tbl):
+        self.state = deepcopy(tbl)
+        self.w, self.h = len(self.state[0]), len(self.state)
+        self.cycles = 0
+
+    def adj(self, p):
+        """Chebyshev neighborhood of point intersected with valid area."""
+        x, y = p
+        for y2 in range(max(0, y - 1), min(self.h, y + 2)):
+            for x2 in range(max(0, x - 1), min(self.w, x + 2)):
+                if x2 == x and y2 == y:
+                    continue
+                yield (x2, y2)
+
+    def area(self):
+        return self.w * self.h
+
+    def points(self):
+        return ((x, y) for y in range(self.h) for x in range(self.w))
+
+    def __getitem__(self, key):
+        x, y = key
+        return self.state[y][x]
+
+    def __setitem__(self, key, value):
+        x, y = key
+        self.state[y][x] = value
+
+    def cycle(self):
+        """Cycle octopi and return number of flashes."""
+        self.cycles += 1
+        sum = 0
+
+        for p in self.points():
+            self[p] += 1
+        spent = set()
+        resets = 1
+        while resets:
+            resets = 0
+            for p in self.points():
+                if self[p] >= 10:
+                    resets += 1
+                    if p not in spent:
+                        spent.add(p)
+                        sum += 1
+                        for p2 in self.adj(p):
+                            if p2 not in spent:
+                                self[p2] += 1
+                    self[p] = 0
+
+        return sum
+
+
+if __name__ == "__main__":
     data = read(lambda x: [int(c) for c in x])
     w, h = len(data[0]), len(data)
 
-    def adj(x, y):
-        """Chebyshev neighborhood of (x, y) intersected with valid area."""
-        for i in [-1, 0, 1]:
-            for j in [-1, 0, 1]:
-                xx, yy = x + i, y + j
-                if (i or j) and xx >= 0 and yy >= 0 and xx < w and yy < h:
-                    yield (xx, yy)
+    # 1
+    s = Squids(data)
+    print(sum(s.cycle() for _ in range(100)))
 
-    state = deepcopy(data)
-    a = 0
-    for _ in range(100):
-        for y in range(h):
-            for x in range(w):
-                state[y][x] += 1
-        spent = set()
-        while True:
-            cycles = 0
-            for y in range(h):
-                for x in range(w):
-                    if state[y][x] >= 10:
-                        cycles += 1
-                        if (x, y) not in spent:
-                            spent.add((x, y))
-                            a += 1
-                            for (x2, y2) in adj(x, y):
-                                if not (x2, y2) in spent:
-                                    state[y2][x2] += 1
-                        state[y][x] = 0
-            if not cycles:
-                break
-
-    print(a)
-
-    state = deepcopy(data)
-    n = 0
-    while True:
-        a = 0
-        for y in range(h):
-            for x in range(w):
-                state[y][x] += 1
-        spent = set()
-        while True:
-            cycles = 0
-            for y in range(h):
-                for x in range(w):
-                    if state[y][x] >= 10:
-                        cycles += 1
-                        if (x, y) not in spent:
-                            spent.add((x, y))
-                            a += 1
-                            for (x2, y2) in adj(x, y):
-                                if not (x2, y2) in spent:
-                                    state[y2][x2] += 1
-                        state[y][x] = 0
-            if not cycles:
-                break
-        n += 1
-        if a == w * h:
-            break
-    print(n)
+    # 2
+    s = Squids(data)
+    while s.cycle() != s.area():
+        pass
+    print(s.cycles)
