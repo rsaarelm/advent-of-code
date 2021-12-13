2723e69a89dad37a43fd6965990c090e4d6d10f4
Risto Saarelma <risto.saarelma@iki.fi>
2021-12-13 07:40:47 +0200

    Runner machinery

--- py/prelude.py
+++ py/prelude.py
@@ -7,11 +7,31 @@ def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 
-def read(parse=str, sep: str = '\n', f: IO = sys.stdin):
+def input_str(*, _ret=[]):
+    """Get input from stdin.
+
+    Can be called repeatedly and will return same value."""
+    if not _ret:
+        _ret.append(sys.stdin.read().rstrip())
+    return _ret[0]
+
+
+def read(parse=str, sep: str = "\n", f: IO = sys.stdin):
     """Read homogeneous `sep` separated input from given file."""
-    return [parse(elt) for elt in f.read().rstrip().split(sep)]
+    return [parse(elt) for elt in input_str().split(sep)]
 
 
 def ints(text: str) -> Tuple[int, ...]:
     """Return tuple of integers extracted from arbitrary text."""
-    return tuple(map(int, re.findall('-?[0-9]+', text)))
+    return tuple(map(int, re.findall("-?[0-9]+", text)))
+
+
+def re_match(r: str, text: str):
+    matches = list(re.search(r, text).groups())
+    for i in range(len(matches)):
+        # Opportunistically convert to integers
+        try:
+            matches[i] = int(matches[i])
+        except ValueError:
+            pass
+    return tuple(matches)
