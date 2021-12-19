80d2b7f820088d2b23181d5c7dc7476e97ef76c8
Risto Saarelma <risto.saarelma@iki.fi>
2021-12-19 10:41:52 +0200

    Day 2021.19 (rs)

---
+++ rs/src/bin/2119.rs
@@ -0,0 +1,187 @@
+use aoc::prelude::*;
+use glam::IVec3;
+use itertools::Itertools;
+use lazy_static::lazy_static;
+use std::{
+    collections::HashSet,
+    convert::TryInto,
+    f32,
+    ops::{Deref, DerefMut},
+};
+
+/// Transform a vector into 24 possible axis-aligned cube orientations.
+fn rotations(vec: impl Into<IVec3>) -> [IVec3; 24] {
+    use glam::{EulerRot, Mat3A};
+
+    lazy_static! {
+        static ref ORIENTATIONS: Vec<Mat3A> = {
+            let mut ret = Vec::new();
+            for &(xa, ya) in &[
+                // Facings:
+                // front,
+                (0.0, 0.0),
+                // sides
+                (0.0, f32::consts::FRAC_PI_2),
+                (0.0, -f32::consts::FRAC_PI_2),
+                // back,
+                (0.0, f32::consts::PI),
+                // up and down.
+                (f32::consts::FRAC_PI_2, 0.0),
+                (-f32::consts::FRAC_PI_2, 0.0),
+            ] {
+                // Different up vectors.
+                for &za in &[0.0, f32::consts::FRAC_PI_2,
+                            f32::consts::PI, -f32::consts::FRAC_PI_2] {
+                    ret.push(Mat3A::from_euler(EulerRot::ZXY,
+                        za, xa, ya));
+                }
+            }
+            ret
+        };
+    }
+
+    let vec = vec.into().as_vec3a();
+
+    ORIENTATIONS
+        .iter()
+        .map(|&m| (m * vec).round().as_ivec3())
+        .collect::<Vec<IVec3>>()
+        .as_slice()
+        .try_into()
+        .unwrap()
+}
+
+#[derive(Clone, Default, Debug)]
+struct Region(Vec<IVec3>);
+
+impl Deref for Region {
+    type Target = Vec<IVec3>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Region {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl Region {
+    /// Return list of vectors which when added to points of current region
+    /// will align them with at least one point of the other region.
+    pub fn match_candidates(&self, other: &Region) -> HashSet<IVec3> {
+        self.iter()
+            .cartesian_product(other.iter())
+            .map(|(&a, &b)| b - a)
+            .collect()
+    }
+
+    pub fn variants(&self) -> [Region; 24] {
+        let mut ret: [Region; 24] = Default::default();
+        for &p in self.iter() {
+            for (i, p) in IntoIterator::into_iter(rotations(p)).enumerate() {
+                ret[i].push(p);
+            }
+        }
+        ret
+    }
+
+    /// Return true if the translation matches this with the other region.
+    pub fn matches(&self, translation: IVec3, other: &Region) -> bool {
+        const EXPECTED_MATCHES: usize = 12;
+
+        // XXX: This doesn't check if this Scanner knows beacons that are in
+        // the other's scan region but not accounted by the other.
+        self.iter()
+            .filter_map(|&v| other.iter().find(|&&p| p == (v + translation)))
+            .count()
+            >= EXPECTED_MATCHES
+    }
+
+    /// Expand self with translated points of other region.
+    pub fn expand(&mut self, translation: IVec3, other: &Region) {
+        for &p in other.iter() {
+            let p = p + translation;
+            if !self.contains(&p) {
+                self.push(p);
+            }
+        }
+    }
+}
+
+fn main() {
+    let regions: Vec<Region> = stdin_string()
+        .trim()
+        .split("\n\n")
+        .map(|chunk| {
+            Region(
+                chunk
+                    .lines()
+                    .skip(1)
+                    .map(fixed_numbers)
+                    .map(|v: [i32; 3]| IVec3::from(v))
+                    .collect::<Vec<IVec3>>(),
+            )
+        })
+        .collect();
+
+    let mut space = regions[0].clone();
+    // Unmatched regions.
+    let mut open: Vec<Region> = regions.iter().skip(1).cloned().collect();
+    // Centers of matched regions. The first one is origin by definition.
+    let mut posns = vec![IVec3::new(0, 0, 0)];
+
+    'top: while !open.is_empty() {
+        for i in 0..open.len() {
+            for r in open[i].variants() {
+                for t in r.match_candidates(&space).into_iter() {
+                    if r.matches(t, &space) {
+                        eprintln!("Index {} of {} matched with {:?}!", i, open.len(), t);
+                        open.swap_remove(i);
+                        space.expand(t, &r);
+                        posns.push(t);
+                        continue 'top;
+                    }
+                }
+            }
+        }
+
+        panic!("Did not find a matching region.");
+    }
+
+    println!("{}", space.len());
+
+    // 2
+    let max_dist = posns
+        .iter()
+        .cartesian_product(posns.iter())
+        .map(|(&a, &b)| (a - b).dot([1, 1, 1].into()))
+        .max()
+        .unwrap_or(0);
+    println!("{}", max_dist);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashSet;
+
+    #[test]
+    fn test_rotations() {
+        let uniqs = |v| {
+            IntoIterator::into_iter(rotations(v))
+                .collect::<HashSet<_>>()
+                .len()
+        };
+
+        assert_eq!(uniqs([0, 0, 0]), 1);
+
+        assert_eq!(uniqs([1, 0, 0]), 6);
+        assert_eq!(uniqs([0, 1, 0]), 6);
+        assert_eq!(uniqs([0, 0, 1]), 6);
+
+        assert_eq!(uniqs([1, 2, 3]), 24);
+    }
+}
