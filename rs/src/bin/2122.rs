795ec5116017fe59e0c4ada80b8b6786be10f6d7
Risto Saarelma <risto.saarelma@iki.fi>
2021-12-22 10:00:44 +0200

    Day 2021.22 (rs)

--- rs/src/bin/2122.rs
+++ rs/src/bin/2122.rs
@@ -1,6 +1,64 @@
-use std::collections::HashSet;
-
 use aoc::prelude::*;
+use itertools::Itertools;
+use std::collections::BTreeSet;
+
+struct ChunkyGrid {
+    xs: Vec<i64>,
+    ys: Vec<i64>,
+    zs: Vec<i64>,
+}
+
+impl ChunkyGrid {
+    pub fn new<'a>(input: impl Iterator<Item = &'a [i64; 6]>) -> ChunkyGrid {
+        let mut xp = BTreeSet::new();
+        let mut yp = BTreeSet::new();
+        let mut zp = BTreeSet::new();
+        for [x0, x1, y0, y1, z0, z1] in input {
+            // Input ranges are end-incusive, grid is end-exclusive so add 1
+            // to end ranges.
+            xp.insert(*x0);
+            xp.insert(*x1 + 1);
+
+            yp.insert(*y0);
+            yp.insert(*y1 + 1);
+
+            zp.insert(*z0);
+            zp.insert(*z1 + 1);
+        }
+        // Deduplicated vecs.
+        let xs: Vec<i64> = xp.into_iter().collect();
+        let ys: Vec<i64> = yp.into_iter().collect();
+        let zs: Vec<i64> = zp.into_iter().collect();
+
+        ChunkyGrid { xs, ys, zs }
+    }
+
+    pub fn map(&self, pos: [i64; 3]) -> [usize; 3] {
+        [
+            self.xs.iter().position(|&a| a == pos[0]).unwrap(),
+            self.ys.iter().position(|&a| a == pos[1]).unwrap(),
+            self.zs.iter().position(|&a| a == pos[2]).unwrap(),
+        ]
+    }
+
+    pub fn volume(&self, pos: [usize; 3]) -> i64 {
+        (self.xs[pos[0] + 1] - self.xs[pos[0]])
+            * (self.ys[pos[1] + 1] - self.ys[pos[1]])
+            * (self.zs[pos[2] + 1] - self.zs[pos[2]])
+    }
+
+    pub fn cells(&self, realspace_cube: &[i64; 6]) -> impl Iterator<Item = [usize; 3]> {
+        let [x0, x1, y0, y1, z0, z1] = *realspace_cube;
+        // Map to gridspace, ranges to end-exclusive.
+        let [cx0, cy0, cz0] = self.map([x0, y0, z0]);
+        let [cx1, cy1, cz1] = self.map([x1 + 1, y1 + 1, z1 + 1]);
+
+        // XXX: Figure out how the hell you get .multi_cartesian_product to work...
+        (cx0..cx1)
+            .cartesian_product((cy0..cy1).cartesian_product(cz0..cz1))
+            .map(|(x, (y, z))| [x, y, z])
+    }
+}
 
 fn main() {
     let mut data: Vec<(bool, [i64; 6])> = Vec::new();
@@ -10,29 +68,28 @@ fn main() {
         data.push((is_on, cub));
     }
 
-    let mut state = HashSet::new();
-    for (bit, p) in &data {
-        eprintln!("{:?}", (bit, (p[1]-p[0]) * (p[3]-p[2]) * (p[5]-p[4])));
-        if p[1] < -50 || p[3] < -50 || p[5] < -50 || p[0] > 50 || p[2] > 50 || p[4] > 50 {
-            continue;
-        }
-        for x in p[0]..=p[1] {
-            for y in p[2]..=p[3] {
-                for z in p[4]..=p[5] {
-                    if x < -50 || y < -50 || z < -50 || x > 50 || y > 50 || z > 50 {
-                        continue;
-                    }
+    let grid = ChunkyGrid::new(data.iter().map(|(_, p)| p));
 
-                    if *bit {
-                        state.insert([x, y, z]);
-                    } else {
-                        state.remove(&[x, y, z]);
-                    }
+    for part in [1, 2] {
+        let mut state = BTreeSet::new();
+        for (bit, cube) in &data {
+            if part == 1 && (cube[1] < -50
+                || cube[3] < -50
+                || cube[5] < -50
+                || cube[0] > 50
+                || cube[2] > 50
+                || cube[4] > 50)
+            {
+                continue;
+            }
+            for p in grid.cells(cube) {
+                if *bit {
+                    state.insert(p);
+                } else {
+                    state.remove(&p);
                 }
             }
         }
+        println!("{}", state.into_iter().map(|c| grid.volume(c)).sum::<i64>());
     }
-
-    println!("{}", state.len());
-    println!("0");
 }
