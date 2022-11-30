eafc2ba8a62a7751a1f84df560a8e96ae8d5028f
Risto Saarelma <risto.saarelma@iki.fi>
2022-11-30 12:49:43 +0200

    Use nightly rustc

--- flake.nix
+++ flake.nix
@@ -4,15 +4,24 @@
     utils.url = "github:numtide/flake-utils";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
 
+    rust-overlay.url = "github:oxalica/rust-overlay";
     naersk.url = "github:nmattia/naersk/master";
   };
 
-  outputs = { self, nixpkgs, utils, naersk, pre-commit-hooks, ... }:
+  outputs =
+    { self, nixpkgs, utils, pre-commit-hooks, rust-overlay, naersk, ... }:
     utils.lib.eachDefaultSystem (system:
       let
         pname = "advent-of-code";
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+        rust = pkgs.rust-bin.nightly.latest.default;
+        naersk-lib = naersk.lib."${system}".override {
+          rustc = rust;
+          cargo = rust;
+        };
       in rec {
         checks = {
           pre-commit-check = pre-commit-hooks.lib.${system}.run {
@@ -34,9 +43,7 @@
               pre-commit
 
               # Rust
-              cargo
-              rustc
-              rustfmt
+              rust
               rust-analyzer
               clippy
               cargo-outdated
