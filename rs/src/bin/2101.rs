ee6f030555b05b9a885b526d99de08ac62dab6dd
Risto Saarelma <risto.saarelma@iki.fi>
2022-12-01 06:46:30 +0200

    Day 2021.01 (rs)

---
+++ rs/src/bin/2101.rs
@@ -0,0 +1,22 @@
+use aoc::prelude::*;
+
+fn increase_count(data: &[u32]) -> usize {
+    data.iter()
+        .zip(data.iter().skip(1))
+        .filter(|(a, b)| b > a)
+        .count()
+}
+
+fn main() {
+    let data: Vec<u32> = stdin_lines().map(|x| x.parse().unwrap()).collect();
+
+    println!("{}", increase_count(&data));
+
+    let data3: Vec<u32> = data
+        .iter()
+        .zip(data.iter().skip(1).zip(data.iter().skip(2)))
+        .map(|(a, (b, c))| a + b + c)
+        .collect();
+
+    println!("{}", increase_count(&data3));
+}
