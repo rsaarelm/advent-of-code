27debd0ef48d0860a503a896dcb67f4e05ee285f
Risto Saarelma <risto.saarelma@iki.fi>
2022-12-13 08:49:23 +0200

    Golf

--- rs/src/bin/2213.rs
+++ rs/src/bin/2213.rs
@@ -11,112 +11,6 @@ enum Item {
 
 use Item::*;
 
-impl Eq for Item {}
-
-impl PartialEq for Item {
-    fn eq(&self, other: &Self) -> bool {
-        self.cmp(other) == Ordering::Equal
-    }
-}
-
-impl Ord for Item {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let (a, b) = (self.clone(), other.clone());
-        let (a, b): (Item, Item) = match (a, b) {
-            (Atom(a), Atom(b)) => return a.cmp(&b), // Early exit
-            (a @ Atom(_), b) => (List(vec![a]), b),
-            (a, b @ Atom(_)) => (a, List(vec![b])),
-            c => c,
-        };
-
-        // They're both lists now.
-        let (List(a), List(b)) = (a, b) else { panic!("Invalid items") };
-        let (mut a, mut b) = (a.as_slice(), b.as_slice());
-
-        loop {
-            if a.is_empty() && b.is_empty() {
-                return Ordering::Equal;
-            } else if a.is_empty() {
-                return Ordering::Less;
-            } else if b.is_empty() {
-                return Ordering::Greater;
-            }
-
-            let c = a[0].cmp(&b[0]);
-            if c == Ordering::Equal {
-                a = &a[1..];
-                b = &b[1..];
-                continue;
-            } else {
-                return c;
-            }
-        }
-    }
-}
-
-impl PartialOrd for Item {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-// For completeness' sake
-impl fmt::Display for Item {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Atom(n) => write!(f, "{}", n),
-            List(a) => {
-                write!(f, "[")?;
-                write!(f, "{}", a.iter().format(","))?;
-                write!(f, "]")
-            }
-        }
-    }
-}
-
-impl FromStr for Item {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        fn parse(mut s: &str) -> Result<(Item, &str), <Item as FromStr>::Err> {
-            // Recursive descent parser.
-            if s.is_empty() {
-                return Err(());
-            }
-
-            // Ignore commas.
-            while s.starts_with(',') {
-                s = &s[1..];
-            }
-
-            let c = s.chars().next().unwrap();
-            if c == '[' {
-                let mut s = &s[1..]; // Drop the opening '['.
-                let mut ret: Vec<Item> = Vec::new();
-                // Parse items from list.
-                while !s.is_empty() && !s.starts_with(']') {
-                    let (item, rest) = parse(s)?;
-                    ret.push(item);
-                    s = rest;
-                }
-                // Drop the closing ].
-                if s.starts_with(']') {
-                    s = &s[1..];
-                }
-
-                Ok((Item::List(ret), s))
-            } else if c.is_ascii_digit() {
-                let number: String = s.chars().take_while(|c| c.is_ascii_digit()).collect();
-                Ok((Item::Atom(number.parse().unwrap()), &s[number.len()..]))
-            } else {
-                Err(())
-            }
-        }
-
-        parse(s).map(|(item, _)| item)
-    }
-}
-
 fn main() {
     let mut packets = vec![Vec::new()];
     for item in stdin_lines() {
@@ -167,3 +61,98 @@ fn main() {
             .product::<usize>()
     );
 }
+
+impl FromStr for Item {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        fn parse(mut s: &str) -> Result<(Item, &str), <Item as FromStr>::Err> {
+            // Recursive descent parser.
+            if s.is_empty() {
+                return Err(());
+            }
+
+            // Ignore commas.
+            while s.starts_with(',') {
+                s = &s[1..];
+            }
+
+            let c = s.chars().next().unwrap();
+            if c == '[' {
+                let mut s = &s[1..]; // Drop the opening '['.
+                let mut ret: Vec<Item> = Vec::new();
+                // Parse items from list.
+                while !s.is_empty() && !s.starts_with(']') {
+                    let (item, rest) = parse(s)?;
+                    ret.push(item);
+                    s = rest;
+                }
+                // Drop the closing ].
+                if s.starts_with(']') {
+                    s = &s[1..];
+                }
+
+                Ok((Item::List(ret), s))
+            } else if c.is_ascii_digit() {
+                let number: String = s.chars().take_while(|c| c.is_ascii_digit()).collect();
+                Ok((Item::Atom(number.parse().unwrap()), &s[number.len()..]))
+            } else {
+                Err(())
+            }
+        }
+
+        parse(s).map(|(item, _)| item)
+    }
+}
+
+// For completeness' sake
+impl fmt::Display for Item {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Atom(n) => write!(f, "{}", n),
+            List(a) => {
+                write!(f, "[")?;
+                write!(f, "{}", a.iter().format(","))?;
+                write!(f, "]")
+            }
+        }
+    }
+}
+
+impl Ord for Item {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let mut atom_list = Vec::new();
+
+        // Convert to slices, using atom_list as scratch space for atomic
+        // items.
+        let (a, b): (&[Item], &[Item]) = match (self, other) {
+            (Atom(a), Atom(b)) => return a.cmp(b), // Early exit
+            (a @ Atom(_), List(b)) => {
+                atom_list.push(a.clone());
+                (atom_list.as_slice(), b.as_slice())
+            }
+            (List(a), b @ Atom(_)) => {
+                atom_list.push(b.clone());
+                (a.as_slice(), atom_list.as_slice())
+            }
+            (List(a), List(b)) => (a.as_slice(), b.as_slice()),
+        };
+
+        // Built-in lexical comparison for slices can take it from here.
+        a.cmp(b)
+    }
+}
+
+impl PartialOrd for Item {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Item {
+    fn eq(&self, other: &Self) -> bool {
+        self.cmp(other) == Ordering::Equal
+    }
+}
+
+impl Eq for Item {}
