95ba869feb62cdee4f8e10ad640beb79529ea11c
Risto Saarelma <risto.saarelma@iki.fi>
2022-12-18 06:58:57 +0200

    Day 2022.18 (rs)

---
+++ rs/src/bin/2218.rs
@@ -0,0 +1,54 @@
+use std::collections::HashSet;
+
+use aoc::prelude::*;
+
+fn main() {
+    let droplets: HashSet<IVec3> = stdin_lines()
+        .map(|line| fixed_numbers::<i32, 3>(&line).into())
+        .collect();
+
+    // Part 1
+
+    let mut surf = 0;
+    for &cube in &droplets {
+        for &d in &SPACE_6 {
+            if !droplets.contains(&(cube + d)) {
+                surf += 1;
+            }
+        }
+    }
+    println!("{surf}");
+
+    // Part 2
+
+    let volume_min = droplets.iter().copied().reduce(IVec3::min).unwrap() - IVec3::splat(1);
+    let volume_max = droplets.iter().copied().reduce(IVec3::max).unwrap() + IVec3::splat(2);
+
+    let open: HashSet<IVec3> = dijkstra_map(
+        |&p| {
+            let droplets = &droplets; // safe to move.
+            SPACE_6.iter().filter_map(move |&d| {
+                let n = d + p;
+                if !droplets.contains(&n) && p.cmpge(volume_min).all() && p.cmplt(volume_max).all()
+                {
+                    Some(n)
+                } else {
+                    None
+                }
+            })
+        },
+        volume_min,
+    )
+    .map(|(n, _)| n)
+    .collect();
+
+    let mut surf = 0;
+    for &cube in &droplets {
+        for &d in &SPACE_6 {
+            if !droplets.contains(&(cube + d)) && open.contains(&(cube + d)) {
+                surf += 1;
+            }
+        }
+    }
+    println!("{surf}");
+}
--- rs/src/prelude.rs
+++ rs/src/prelude.rs
@@ -11,7 +11,7 @@ pub use memoize::memoize;
 use regex::Regex;
 use rustc_hash::FxHashSet;
 
-pub use glam::{ivec2, IVec2};
+pub use glam::{ivec2, IVec2, ivec3, IVec3};
 
 pub use crate::ocr::{PointCloud, points};
 
@@ -26,6 +26,15 @@ pub const DIR_8: [IVec2; 8] = [
     ivec2(-1, -1),
     ivec2(0, -1),
     ivec2(1, -1),
+];
+
+pub const SPACE_6: [IVec3; 6] = [
+    ivec3(1, 0, 0),
+    ivec3(-1, 0, 0),
+    ivec3(0, 1, 0),
+    ivec3(0, -1, 0),
+    ivec3(0, 0, 1),
+    ivec3(0, 0, -1),
 ];
 
 pub fn stdin_string() -> String {
