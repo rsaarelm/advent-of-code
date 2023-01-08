f2dd17d8399f842ad39132f27ffe7f48635abce6
Risto Saarelma <risto.saarelma@iki.fi>
2023-01-08 20:26:02 +0200

    Day 2015.14 (rs)

---
+++ rs/src/bin/1514.rs
@@ -0,0 +1,31 @@
+use aoc::prelude::*;
+
+fn pos(speed: i32, burst: i32, rest: i32, t: i32) -> i32 {
+    let period = burst + rest;
+    (t / period) * speed * burst + (t % period).min(burst) * speed
+}
+
+fn main() {
+    let input: Vec<[i32; 3]> = stdin_lines().map(fixed_numbers).collect();
+
+    println!(
+        "{}",
+        input
+            .iter()
+            .map(|[s, b, r]| pos(*s, *b, *r, 2503))
+            .max()
+            .unwrap()
+    );
+
+    let mut score = vec![0; input.len()];
+    for t in 1..2504 {
+        let ps: Vec<i32> = input.iter().map(|[s, b, r]| pos(*s, *b, *r, t)).collect();
+        let lead = *ps.iter().max().unwrap();
+        for (i, s) in score.iter_mut().enumerate() {
+            if ps[i] == lead {
+                *s += 1;
+            }
+        }
+    }
+    println!("{}", score.iter().max().unwrap());
+}
