65367a6e5b9dba1cc0f4f40969bb8dc4dc64c7db
Risto Saarelma <risto.saarelma@iki.fi>
2023-01-10 04:13:27 +0200

    Day 2015.20 (rs)

---
+++ rs/src/bin/1520.rs
@@ -0,0 +1,21 @@
+use aoc::prelude::*;
+
+fn deliver(target: usize, c: usize, elfstop: usize) -> usize {
+    let mut bins = vec![0; target / 10];  // Guesstimated contracting.
+    let n = bins.len();
+    for i in 1..n {
+        for j in (i..n).step_by(i).take(elfstop) {
+            bins[j] += i * c;
+            if bins[j] >= target {
+                break;
+            }
+        }
+    }
+    bins.iter().position(|&n| n >= target).expect("Failed to reach target")
+}
+
+fn main() {
+    let target = stdin_string().parse::<usize>().unwrap();
+    println!("{}", deliver(target, 10, usize::MAX));
+    println!("{}", deliver(target, 11, 50));
+}
