6b6310a883ad33fae4b8994cff79a737b44ca78f
Risto Saarelma <risto.saarelma@iki.fi>
2023-05-11 12:06:17 +0300

    Day 2017.10 (rs)

--- rs/examples/1710.rs
+++ rs/examples/1710.rs
@@ -1,6 +1,56 @@
 use aoc::prelude::*;
+use itertools::Itertools;
+
+fn reverse(state: &mut Vec<u32>, pos: usize, n: usize) {
+    let len = state.len();
+    for i in 0..(n / 2) {
+        let (j, k) = ((pos + i) % len, (pos + n - 1 - i) % len);
+        (state[j], state[k]) = (state[k], state[j]);
+    }
+}
+
+fn knot_hash(input: &[u8]) -> Vec<u8> {
+    let input: Vec<u8> =
+        input.iter().copied().chain([17, 31, 73, 47, 23]).collect();
+    let mut state: Vec<u32> = (0..=255).collect();
+    let mut pos = 0;
+    let mut skip = 0;
+    for _ in 0..64 {
+        for &n in input.iter() {
+            let n = n as usize;
+            reverse(&mut state, pos, n);
+            pos += skip + n;
+            skip += 1;
+        }
+    }
+
+    state
+        .into_iter()
+        .chunks(16)
+        .into_iter()
+        .map(|c| c.reduce(|acc, e| acc ^ e).unwrap_or(0) as u8)
+        .collect()
+}
 
 fn main() {
-    // TODO
-    eprintln!("{}", stdin_string());
+    let s = stdin_string();
+    let input: Vec<usize> = numbers(&s);
+    let mut state: Vec<u32> = (0..=255).collect();
+
+    let mut pos = 0;
+    for (skip, &n) in input.iter().enumerate() {
+        reverse(&mut state, pos, n);
+        pos += skip + n;
+    }
+    println!("{}", state[0] * state[1]);
+
+    // Part 2
+
+    // Unit test
+    assert_eq!(
+        bytes_to_hex(&knot_hash(&Vec::new())),
+        "a2582a3a0e66e6e86e3812dcb672a272"
+    );
+
+    println!("{}", bytes_to_hex(&knot_hash(s.as_bytes())));
 }
