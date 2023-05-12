6ff76a4bd009900052d17f1455c49cb0ba38db1b
Risto Saarelma <risto.saarelma@iki.fi>
2023-05-12 13:32:41 +0300

    Day 2017.12 (rs)

--- rs/examples/1712.rs
+++ rs/examples/1712.rs
@@ -1,6 +1,28 @@
 use aoc::prelude::*;
 
 fn main() {
-    // TODO
-    eprintln!("{}", stdin_string());
+    let input: Vec<Vec<usize>> = stdin_lines()
+        .map(|line| {
+            let mut x = numbers(line);
+            x.remove(0);
+            x
+        })
+        .collect();
+
+    // Part 1
+    println!(
+        "{}",
+        dijkstra_map(|&n| input[n].iter().copied(), &0).count()
+    );
+
+    // Part 2
+    let mut pipes: HashSet<usize> = (0..input.len()).collect();
+    let mut count = 0;
+    while let Some(root) = pipes.pop() {
+        for (a, _) in dijkstra_map(|&n| input[n].iter().copied(), &root) {
+            pipes.remove(&a);
+        }
+        count += 1;
+    }
+    println!("{count}");
 }
