7829fee717b819a370675fa0c179190d6a8bbae4
Risto Saarelma <risto.saarelma@iki.fi>
2023-05-12 20:23:40 +0300

    Day 2017.13 (rs)

--- rs/examples/1713.rs
+++ rs/examples/1713.rs
@@ -1,6 +1,31 @@
 use aoc::prelude::*;
 
+fn sweep(input: &HashMap<usize, usize>, offset: usize) -> (bool, usize) {
+    let mut threat = 0;
+    let mut caught = false;
+    for (t, range) in input {
+        let span = 2 * range - 2;
+        if (t + offset) % span == 0 {
+            threat += t * range;
+            caught = true;
+        }
+    }
+    (caught, threat)
+}
+
 fn main() {
-    // TODO
-    eprintln!("{}", stdin_string());
+    let mut input = HashMap::default();
+    for line in stdin_lines() {
+        let [a, b]: [usize; 2] = fixed_numbers(line);
+        input.insert(a, b);
+    }
+
+    println!("{}", sweep(&input, 0).1);
+
+    for i in 0.. {
+        if !sweep(&input, i).0 {
+            println!("{i}");
+            break;
+        }
+    }
 }
