ef653fb6d88e98374040def3977fc507529f42fc
Risto Saarelma <risto.saarelma@iki.fi>
2023-05-13 08:20:39 +0300

    Day 2017.14 (rs)

--- rs/examples/1710.rs
+++ rs/examples/1710.rs
@@ -1,5 +1,7 @@
 use aoc::prelude::*;
-use itertools::Itertools;
+
+mod knot_hash;
+use knot_hash::knot_hash;
 
 fn reverse(state: &mut Vec<u32>, pos: usize, n: usize) {
     let len = state.len();
@@ -7,29 +9,6 @@ fn reverse(state: &mut Vec<u32>, pos: usize, n: usize) {
         let (j, k) = ((pos + i) % len, (pos + n - 1 - i) % len);
         (state[j], state[k]) = (state[k], state[j]);
     }
-}
-
-fn knot_hash(input: &[u8]) -> Vec<u8> {
-    let input: Vec<u8> =
-        input.iter().copied().chain([17, 31, 73, 47, 23]).collect();
-    let mut state: Vec<u32> = (0..=255).collect();
-    let mut pos = 0;
-    let mut skip = 0;
-    for _ in 0..64 {
-        for &n in input.iter() {
-            let n = n as usize;
-            reverse(&mut state, pos, n);
-            pos += skip + n;
-            skip += 1;
-        }
-    }
-
-    state
-        .into_iter()
-        .chunks(16)
-        .into_iter()
-        .map(|c| c.reduce(|acc, e| acc ^ e).unwrap_or(0) as u8)
-        .collect()
 }
 
 fn main() {
--- rs/examples/1714.rs
+++ rs/examples/1714.rs
@@ -1,6 +1,42 @@
 use aoc::prelude::*;
+use bitvec::prelude::*;
+
+mod knot_hash;
+use knot_hash::knot_hash;
 
 fn main() {
-    // TODO
-    eprintln!("{}", stdin_string());
+    let bounds = area(128, 128);
+
+    let input = stdin_string();
+    let inner: Vec<u8> = (0..128)
+        .flat_map(|y| knot_hash(format!("{input}-{y}").as_bytes()))
+        .collect();
+    let buf = inner.view_bits::<Msb0>();
+
+    println!("{}", buf.count_ones());
+
+    // Part 2
+
+    let mut groups = 0;
+
+    // Cells of already encountered groups.
+    let mut seen = HashSet::default();
+
+    for p in bounds.into_iter() {
+        if buf[bounds.idx(p)] && !seen.contains(&p) {
+            groups += 1;
+            // Flood fill the rest of the new group and mark it as seen.
+            for (p, _) in dijkstra_map(
+                |&p| {
+                    neighbors_4(p)
+                        .filter(|&p| bounds.contains(p) && buf[bounds.idx(p)])
+                },
+                &p,
+            ) {
+                seen.insert(p);
+            }
+        }
+    }
+
+    println!("{groups}");
 }
