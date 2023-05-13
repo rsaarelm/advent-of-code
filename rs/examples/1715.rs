40e3e4556eaada07f501b9f69f64ae05da8c3f91
Risto Saarelma <risto.saarelma@iki.fi>
2023-05-13 19:03:58 +0300

    Day 2017.15 (rs)

--- rs/examples/1715.rs
+++ rs/examples/1715.rs
@@ -1,6 +1,38 @@
 use aoc::prelude::*;
 
+fn gen(mut seed: u64, factor: u64) -> impl Iterator<Item = u64> {
+    std::iter::from_fn(move || {
+        let ret = Some(seed);
+        seed *= factor;
+        seed %= 0x7fffffff;
+        ret
+    })
+}
+
 fn main() {
-    // TODO
-    eprintln!("{}", stdin_string());
+    let input: Vec<u64> =
+        parsed_stdin_lines(r"Generator . starts with (.*)").collect();
+
+    let a = || gen(input[0], 16807);
+    let b = || gen(input[1], 48271);
+
+    let mut matches = 0;
+    for (a, b) in a().zip(b()).take(40_000_000) {
+        if a & 0xffff == b & 0xffff {
+            matches += 1;
+        }
+    }
+    println!("{matches}");
+
+    let mut matches = 0;
+    for (a, b) in a()
+        .filter(|a| a % 4 == 0)
+        .zip(b().filter(|b| b % 8 == 0))
+        .take(5_000_000)
+    {
+        if a & 0xffff == b & 0xffff {
+            matches += 1;
+        }
+    }
+    println!("{matches}");
 }
