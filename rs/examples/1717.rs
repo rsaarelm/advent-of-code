f5c6c7e2a66f3fdda9aab16b20837c1179dd1224
Risto Saarelma <risto.saarelma@iki.fi>
2023-05-15 07:58:33 +0300

    Day 2017.17 (rs)

--- rs/examples/1717.rs
+++ rs/examples/1717.rs
@@ -1,6 +1,26 @@
 use aoc::prelude::*;
 
 fn main() {
-    // TODO
-    eprintln!("{}", stdin_string());
+    let step: usize = stdin_string().parse().unwrap();
+
+    let mut p = 0;
+    let mut buf = vec![0];
+
+    for i in 1..=2017 {
+        p = (p + step + 1) % i;
+        buf.insert(p, i);
+    }
+    println!("{}", buf[(p + 1) % buf.len()]);
+
+    // P2
+    // The trick: Zero value never moves from index 0.
+    let mut p = 0;
+    let mut result = 0;
+    for i in 1..=50_000_000 {
+        p = (p + step + 1) % i;
+        if p == 0 {
+            result = i;
+        }
+    }
+    println!("{result}");
 }
