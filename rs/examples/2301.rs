a0e62ee44000bfd46e22db50e09d02d63b3f8ce8
Risto Saarelma <risto.saarelma@iki.fi>
2023-12-01 12:45:25 +0200

    Tighten 2023.01 (rs)

--- rs/examples/2301.rs
+++ rs/examples/2301.rs
@@ -5,18 +5,14 @@ fn digit1(s: &str) -> Option<u32> {
 }
 
 fn digit2(s: &str) -> Option<u32> {
-    match s {
-        s if s.starts_with("one") => Some(1),
-        s if s.starts_with("two") => Some(2),
-        s if s.starts_with("three") => Some(3),
-        s if s.starts_with("four") => Some(4),
-        s if s.starts_with("five") => Some(5),
-        s if s.starts_with("six") => Some(6),
-        s if s.starts_with("seven") => Some(7),
-        s if s.starts_with("eight") => Some(8),
-        s if s.starts_with("nine") => Some(9),
-        s => digit1(s),
-    }
+    [
+        "💩", "one", "two", "three", "four", "five", "six", "seven", "eight",
+        "nine",
+    ]
+    .into_iter()
+    .enumerate()
+    .find_map(|(n, w)| s.starts_with(w).then_some(n as u32))
+    .or_else(|| digit1(s))
 }
 
 fn main() {
