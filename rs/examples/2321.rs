6cae9d00ae911542b6532e1a2f37e66286234009
Risto Saarelma <risto.saarelma@iki.fi>
2023-12-21 14:11:08 +0200

    Day 2023.21 (rs)

---
+++ rs/examples/2321.rs
@@ -0,0 +1,93 @@
+use aoc::prelude::*;
+use polyfit_rs::polyfit_rs::polyfit;
+
+fn rim(
+    bounds: &Rect<i32>,
+    blocks: &HashSet<IVec2>,
+    start: IVec2,
+    n: usize,
+) -> usize {
+    dijkstra_map(
+        |&p| {
+            neighbors_4(p).filter(|&p| {
+                !blocks.contains(&bounds.get(bounds.idx(p)).into())
+            })
+        },
+        &start,
+    )
+    .skip_while(|(_, d)| *d < n)
+    .take_while(|(_, d)| *d == n)
+    .count()
+}
+
+fn area(
+    bounds: &Rect<i32>,
+    blocks: &HashSet<IVec2>,
+    start: IVec2,
+    n: usize,
+) -> usize {
+    ((n % 2)..=n)
+        .step_by(2)
+        .map(|n| rim(bounds, blocks, start, n))
+        .sum::<usize>()
+}
+
+fn main() {
+    let (bounds, buf) = grid(stdin_string());
+    let mut blocks = HashSet::default();
+    let mut start = ivec2(0, 0);
+    for (i, c) in buf.iter().enumerate() {
+        let pos = IVec2::from(bounds.get(i));
+        match *c {
+            '#' => {
+                blocks.insert(pos);
+            }
+            'S' => {
+                start = pos;
+            }
+            _ => {}
+        }
+    }
+
+    let n = if bounds.width() == 11 {
+        // Example
+        6
+    } else {
+        64
+    };
+
+    println!("{}", area(&bounds, &blocks, start, n));
+
+    // P2
+    const STEPS: usize = 26501365;
+
+    // Area repeats in a pattern for every map width multiple.
+    let n = bounds.width() as usize;
+    let k = STEPS % n;
+
+    // Grab some data points. We need three points to derive a quadratic
+    // polynomial.
+    let mut x = Vec::new();
+    let mut y = Vec::new();
+
+    let mut area = 0;
+    for i in (1..).step_by(2) {
+        area += rim(&bounds, &blocks, start, i);
+        if i % n == k {
+            x.push(i as f64);
+            y.push(area as f64);
+        }
+        if x.len() == 3 {
+            break;
+        }
+    }
+
+    // And then use a crate to fit it because lazy.
+    let [c, b, a] = &polyfit(&x, &y, 2).unwrap()[..] else {
+        panic!()
+    };
+
+    // And calculate ax^2 + bx + c for the target step count.
+    let s = STEPS as f64;
+    println!("{}", (a * s.powi(2) + b * s + c) as u64);
+}
