8211168c2b10b717cca101dd2f7c0fd3f0eceac6
Risto Saarelma <risto.saarelma@iki.fi>
2024-12-03 07:18:11 +0200

    Day 2024.03 (rs)

--- rs/examples/2403.rs
+++ rs/examples/2403.rs
@@ -1,7 +1,32 @@
+use regex::Regex;
+
 use aoc::prelude::*;
 
-fn main() {
-    for line in stdin_lines() {
-        println!("{line}");
+fn run(p2: bool, text: &str) -> i32 {
+    // Parameter-less ops have dummy capture groups to make the regex valid.
+    let op = Regex::new(r"(mul)\((\d+),(\d+)\)|(do)\(()()\)|(don't)\(()()\)")
+        .unwrap();
+    let mut is_active = true;
+    let mut ret = 0;
+
+    for (_, [op, a, b]) in op.captures_iter(text).map(|c| c.extract()) {
+        match op {
+            "do" => is_active = true,
+            "don't" => is_active = false,
+            "mul" if is_active || !p2 => {
+                let (a, b): (i32, i32) =
+                    (a.parse().unwrap(), b.parse().unwrap());
+                ret += a * b;
+            }
+            _ => {}
+        }
     }
+
+    ret
+}
+
+fn main() {
+    let text = stdin_string();
+    println!("{}", run(false, &text));
+    println!("{}", run(true, &text));
 }
