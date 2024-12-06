c43fb94ac1103bd109d854a547d2ac6bd26722f1
Risto Saarelma <risto.saarelma@iki.fi>
2024-12-06 07:16:22 +0200

    Day 2024.06 (rs)

--- rs/examples/2406.rs
+++ rs/examples/2406.rs
@@ -1,7 +1,62 @@
 use aoc::prelude::*;
 
-fn main() {
-    for line in stdin_lines() {
-        println!("{line}");
+// Return length for exit, None if path loops.
+fn path(bounds: &Rect<i32>, map: &HashSet<IVec2>, mut pos: IVec2) -> Option<HashMap<IVec2, Vec<IVec2>>> {
+    let mut dir = ivec2(0, -1);
+    // Footprint, tracking directions it was traversed in.
+    let mut posns: HashMap<IVec2, Vec<IVec2>> = HashMap::default();
+
+    loop {
+        let prevs = posns.entry(pos).or_default();
+        if prevs.contains(&dir) {
+            // Loop detected, bail.
+            return None;
+        }
+        prevs.push(dir);
+
+        // Turn away from obstacles (will loop forever if completely boxed in)
+        while map.contains(&(pos + dir)) {
+            dir = dir.cw();
+        }
+        if !bounds.contains(pos + dir) {
+            return Some(posns);
+        }
+        pos += dir;
     }
+}
+
+fn main() {
+    let mut map: HashSet<IVec2> = Default::default();
+    let mut pos: IVec2 = Default::default();
+    let mut bounds: Rect<i32> = Default::default();
+
+    for ([x, y], c) in stdin_grid_iter() {
+        bounds.p1[0] = bounds.p1[0].max(x + 1);
+        bounds.p1[1] = bounds.p1[1].max(y + 1);
+        match c {
+            '#' => {
+                map.insert(ivec2(x, y));
+            }
+            '^' => {
+                pos = ivec2(x, y);
+            }
+            '.' => {}
+            _ => panic!("Bad char {c}"),
+        }
+    }
+
+    let default_path = path(&bounds, &map, pos).unwrap();
+
+    println!("{}", default_path.len());
+
+    let mut p2 = 0;
+    for &p in default_path.keys() {
+        let mut map2 = map.clone();
+        map2.insert(p);
+        if path(&bounds, &map2, pos).is_none() {
+            p2 += 1;
+        }
+    }
+
+    println!("{p2}");
 }
