4304872efafd8a4c90a311b75a23582d64b38f11
Risto Saarelma <risto.saarelma@iki.fi>
2024-12-06 13:45:58 +0200

    Day 2024.06 (rs) nicer solution

--- rs/examples/2406.rs
+++ rs/examples/2406.rs
@@ -1,32 +1,20 @@
 use aoc::prelude::*;
+use itertools::Itertools;
 
-// Return length for exit, None if path loops.
 fn path(
-    bounds: &Rect<i32>,
     map: &HashSet<IVec2>,
     mut pos: IVec2,
-) -> Option<HashMap<IVec2, Vec<IVec2>>> {
+) -> impl Iterator<Item = IVec2> + '_ {
     let mut dir = ivec2(0, -1);
-    // Footprint, tracking directions it was traversed in.
-    let mut posns: HashMap<IVec2, Vec<IVec2>> = HashMap::default();
-
-    loop {
-        let prevs = posns.entry(pos).or_default();
-        if prevs.contains(&dir) {
-            // Loop detected, bail.
-            return None;
-        }
-        prevs.push(dir);
-
+    std::iter::from_fn(move || {
         // Turn away from obstacles (will loop forever if completely boxed in)
         while map.contains(&(pos + dir)) {
             dir = dir.cw();
         }
-        if !bounds.contains(pos + dir) {
-            return Some(posns);
-        }
+
         pos += dir;
-    }
+        Some(pos - dir)
+    })
 }
 
 fn main() {
@@ -47,16 +35,31 @@ fn main() {
         }
     }
 
-    let default_path = path(&bounds, &map, pos).unwrap();
+    let default_path: HashSet<_> = path(&map, pos)
+        .take_while(|&p| bounds.contains(p))
+        .collect();
 
     println!("{}", default_path.len());
 
     let mut p2 = 0;
-    for &p in default_path.keys() {
+    for &p in &default_path {
         let mut map2 = map.clone();
         map2.insert(p);
-        if path(&bounds, &map2, pos).is_none() {
-            p2 += 1;
+
+        let mut seen: HashSet<(IVec2, IVec2)> = HashSet::default();
+        // Get position + direction by tracking consecutive pairs of steps,
+        // this lets us see when we've entered a loop.
+        for step in path(&map2, pos)
+            .take_while(|&p| bounds.contains(p))
+            .tuple_windows()
+        {
+            if seen.contains(&step) {
+                // Loop detected, mark this one down and continue scan.
+                p2 += 1;
+                break;
+            }
+
+            seen.insert(step);
         }
     }
 
