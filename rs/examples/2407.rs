f42056275541e0c8bd07d45df64549df4fd5c79b
Risto Saarelma <risto.saarelma@iki.fi>
2024-12-07 11:58:06 +0200

    Golf

--- rs/examples/2407.rs
+++ rs/examples/2407.rs
@@ -1,34 +1,15 @@
 use aoc::prelude::*;
 use itertools::Itertools;
 
-#[derive(Copy, Clone, Debug)]
-pub enum Op {
-    Add,
-    Mul,
-    Cat,
-}
-
-use Op::*;
-
-impl Op {
-    pub fn apply(self, a: i64, b: i64) -> i64 {
-        match self {
-            Add => a + b,
-            Mul => a * b,
-            Cat => format!("{a}{b}").parse().unwrap(),
-        }
-    }
-}
-
-fn find(a: i64, eqn: &[i64], ops: &[Op]) -> bool {
-    // Generate all op permuatations for the given equation.
+fn find(a: i64, eqn: &[i64], ops: &[Box<dyn Fn(i64, i64) -> i64>]) -> bool {
+    // Generate all op permutations for the given equation.
     'search: for ops in
         std::iter::repeat_n(ops, eqn.len() - 1).multi_cartesian_product()
     {
         let mut ret = eqn[0];
 
         for (i, e) in eqn.iter().skip(1).enumerate() {
-            ret = ops[i].apply(ret, *e);
+            ret = ops[i](ret, *e);
             if ret > a {
                 continue 'search;
             }
@@ -43,13 +24,19 @@ fn find(a: i64, eqn: &[i64], ops: &[Op]) -> bool {
 }
 
 fn main() {
+    let ops: [Box<dyn Fn(i64, i64) -> i64>; 3] = [
+        Box::new(|a, b| a + b),
+        Box::new(|a, b| a * b),
+        Box::new(|a, b| format!("{a}{b}").parse().unwrap()),
+    ];
+
     let input: Vec<Vec<i64>> = stdin_lines().map(numbers).collect();
-    for ops in [vec![Add, Mul], vec![Add, Mul, Cat]] {
+    for ops in [&ops[0..2], &ops[0..3]] {
         println!(
             "{}",
             input
                 .iter()
-                .filter_map(|a| find(a[0], &a[1..], &ops).then_some(a[0]))
+                .filter_map(|a| find(a[0], &a[1..], ops).then_some(a[0]))
                 .sum::<i64>()
         );
     }
