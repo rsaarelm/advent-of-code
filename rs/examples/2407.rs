a1e4ad6fd6a5e998c89a22ea83e2dbe492d6247b
Risto Saarelma <risto.saarelma@iki.fi>
2024-12-07 09:21:17 +0200

    Be a bit nicer and use an enum

--- rs/examples/2407.rs
+++ rs/examples/2407.rs
@@ -1,25 +1,34 @@
 use aoc::prelude::*;
 use itertools::Itertools;
 
-const ADD: usize = 0;
-const MUL: usize = 1;
-const CAT: usize = 2;
+#[derive(Copy, Clone, Debug)]
+pub enum Op {
+    Add,
+    Mul,
+    Cat,
+}
 
-fn find(a: i64, eqn: &[i64], n_ops: usize) -> bool {
-    // Generate all permutations of ops from ADD to n_ops-1 for the current
-    // equation.
+use Op::*;
+
+impl Op {
+    pub fn apply(self, a: i64, b: i64) -> i64 {
+        match self {
+            Add => a + b,
+            Mul => a * b,
+            Cat => format!("{a}{b}").parse().unwrap(),
+        }
+    }
+}
+
+fn find(a: i64, eqn: &[i64], ops: &[Op]) -> bool {
+    // Generate all op permuatations for the given equation.
     'search: for ops in
-        std::iter::repeat_n(0..n_ops, eqn.len() - 1).multi_cartesian_product()
+        std::iter::repeat_n(ops, eqn.len() - 1).multi_cartesian_product()
     {
         let mut ret = eqn[0];
 
         for (i, e) in eqn.iter().skip(1).enumerate() {
-            match ops[i] {
-                ADD => ret += e,
-                MUL => ret *= e,
-                CAT => ret = format!("{ret}{e}").parse().unwrap(),
-                x => panic!("Bad op {x}"),
-            }
+            ret = ops[i].apply(ret, *e);
             if ret > a {
                 continue 'search;
             }
@@ -35,12 +44,12 @@ fn find(a: i64, eqn: &[i64], n_ops: usize) -> bool {
 
 fn main() {
     let input: Vec<Vec<i64>> = stdin_lines().map(numbers).collect();
-    for ops in [2, 3] {
+    for ops in [vec![Add, Mul], vec![Add, Mul, Cat]] {
         println!(
             "{}",
             input
                 .iter()
-                .filter_map(|a| find(a[0], &a[1..], ops).then_some(a[0]))
+                .filter_map(|a| find(a[0], &a[1..], &ops).then_some(a[0]))
                 .sum::<i64>()
         );
     }
