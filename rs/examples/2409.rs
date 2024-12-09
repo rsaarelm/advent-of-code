dd55f2f8af6442b84b402cd854b0244951983f52
Risto Saarelma <risto.saarelma@iki.fi>
2024-12-09 11:46:54 +0200

    Refactor 2024.09 (rs)

--- rs/examples/2409.rs
+++ rs/examples/2409.rs
@@ -1,100 +1,98 @@
 use aoc::prelude::*;
 
-fn p1(input: &str) {
-    let mut disk = Vec::new();
-    let mut holes = Vec::new();
-
-    for (i, c) in input.chars().enumerate() {
-        let is_file = i % 2 == 0;
-        let len: usize = c.to_digit(10).unwrap() as usize;
-
-        if is_file {
-            let id = (i / 2) as i64;
-            for _ in 0..len {
-                disk.push(id);
-            }
-        } else {
-            for _ in 0..len {
-                holes.push(disk.len());
-                disk.push(-1);
-            }
-        }
-    }
-
-    holes.reverse(); // For popping.
-
-    while let Some(hole) = holes.pop() {
-        while disk[disk.len() - 1] == -1 {
-            disk.pop();
-        }
-
-        if hole >= disk.len() {
-            break;
-        }
-
-        let n = disk.pop().unwrap();
-        disk[hole] = n;
-    }
-
-    println!(
-        "{}",
-        disk.iter()
-            .enumerate()
-            .filter(|(_, b)| **b != -1)
-            .map(|(a, b)| a as i64 * b)
-            .sum::<i64>()
-    );
+#[derive(Clone)]
+struct Disk {
+    // [(begin, length, id)]
+    files: Vec<(usize, usize, usize)>,
+    // [(begin, length)]
+    holes: Vec<(usize, usize)>,
 }
 
-fn p2(input: &str) {
-    fn checksum(disk: &[(usize, usize, usize)]) -> usize {
-        disk.iter()
+impl FromStr for Disk {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut p = 0;
+        let mut files = Vec::new();
+        let mut holes = Vec::new();
+
+        for (i, c) in s.chars().enumerate() {
+            let len: usize = c.to_digit(10).unwrap() as usize;
+            if i % 2 == 0 {
+                files.push((p, len, i / 2));
+                p += len;
+            } else {
+                holes.push((p, len));
+                p += len;
+            }
+        }
+
+        Ok(Disk { files, holes })
+    }
+}
+
+impl Disk {
+    pub fn defrag(&mut self, allow_partial: bool) -> &mut Self {
+        for i in (0..self.files.len()).rev() {
+            let (pos, mut len, id) = self.files[i];
+
+            while let Some((p, allocated)) =
+                self.malloc(len, pos, allow_partial)
+            {
+                len -= allocated;
+                self.files.push((p, allocated, id));
+            }
+
+            self.files[i] = (pos, len, id);
+        }
+
+        self
+    }
+
+    fn malloc(
+        &mut self,
+        size: usize,
+        max_pos: usize,
+        allow_partial: bool,
+    ) -> Option<(usize, usize)> {
+        if size == 0 {
+            return None;
+        }
+
+        for (p, s) in &mut self.holes {
+            if *p > max_pos {
+                return None;
+            }
+
+            if *s == 0 {
+                continue;
+            } else if *s >= size {
+                let ret = Some((*p, size));
+                *p += size;
+                *s -= size;
+                return ret;
+            } else if allow_partial {
+                let ret = Some((*p, *s));
+                *s = 0;
+                return ret;
+            }
+        }
+        None
+    }
+
+    pub fn checksum(&self) -> usize {
+        self.files
+            .iter()
             .flat_map(|(begin, length, id)| {
                 (*begin..(*begin + *length)).map(|i| i * *id)
             })
             .sum()
     }
-
-    let mut disk = Vec::new(); // [(begin, length, id)]
-    let mut holes = Vec::new(); // [(begin, length)]
-    let mut p = 0; // Current writing pos.
-
-    for (i, c) in input.chars().enumerate() {
-        let is_file = i % 2 == 0;
-        let len: usize = c.to_digit(10).unwrap() as usize;
-
-        if is_file {
-            let id = i / 2;
-            disk.push((p, len, id));
-            p += len;
-        } else {
-            holes.push((p, len));
-            p += len;
-        }
-    }
-
-    // We don't need to refragment to the original disk.
-    let mut refrag = Vec::new();
-
-    'defrag: for i in (0..disk.len()).rev() {
-        let (pos, len, id) = disk[i];
-
-        for (hole_pos, hole_len) in &mut holes {
-            if *hole_len >= len && *hole_pos < pos {
-                refrag.push((*hole_pos, len, id));
-                *hole_pos += len;
-                *hole_len -= len;
-                disk.swap_remove(i);
-                continue 'defrag;
-            }
-        }
-    }
-
-    println!("{}", checksum(&disk) + checksum(&refrag));
 }
 
 fn main() {
-    let input = stdin_string();
-    p1(&input);
-    p2(&input);
+    let disk: Disk = stdin_string().parse().unwrap();
+
+    println!("{}", disk.clone().defrag(true).checksum());
+    println!("{}", disk.clone().defrag(false).checksum());
 }
