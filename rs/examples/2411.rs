8f67d6a54c52a06991b51b9d484a210f9443ca21
Risto Saarelma <risto.saarelma@iki.fi>
2024-12-11 09:20:46 +0200

    Golf

--- rs/examples/2411.rs
+++ rs/examples/2411.rs
@@ -1,36 +1,32 @@
 use aoc::prelude::*;
 
-fn blink(stones: &mut HashMap<u64, usize>) {
+fn blink(stones: &HashMap<u64, usize>) -> HashMap<u64, usize> {
+    let mut new_stones = HashMap::default();
     for (i, n) in stones.iter().map(|(&a, &b)| (a, b)).collect::<Vec<_>>() {
-        if stones[&i] == n {
-            stones.remove(&i);
-        } else {
-            stones.insert(i, stones[&i] - n);
-        }
-
         let digits = format!("{}", i);
         if i == 0 {
-            *stones.entry(1).or_default() += n;
+            *new_stones.entry(1).or_default() += n;
         } else if digits.len() % 2 == 0 {
             let (a, b) = (
                 digits[0..digits.len() / 2].parse::<u64>().unwrap(),
                 digits[digits.len() / 2..].parse::<u64>().unwrap(),
             );
-            *stones.entry(a).or_default() += n;
-            *stones.entry(b).or_default() += n;
+            *new_stones.entry(a).or_default() += n;
+            *new_stones.entry(b).or_default() += n;
         } else {
-            *stones.entry(i * 2024).or_default() += n;
+            *new_stones.entry(i * 2024).or_default() += n;
         }
     }
+    new_stones
 }
 
 fn main() {
     let mut stones: HashMap<u64, usize> =
         histogram(numbers(stdin_string())).collect();
 
-    (0..25).for_each(|_| blink(&mut stones));
+    (0..25).for_each(|_| stones = blink(&stones));
     println!("{}", stones.values().sum::<usize>());
 
-    (25..75).for_each(|_| blink(&mut stones));
+    (25..75).for_each(|_| stones = blink(&stones));
     println!("{}", stones.values().sum::<usize>());
 }
