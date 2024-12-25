bbbd94c7c579d36312abe7af5520c2b80daa0fb9
Risto Saarelma <risto.saarelma@iki.fi>
2024-12-25 08:01:29 +0200

    Day 2024.25 (rs)

--- rs/examples/2425.rs
+++ rs/examples/2425.rs
@@ -1,7 +1,29 @@
 use aoc::prelude::*;
 
 fn main() {
-    for line in stdin_lines() {
-        println!("{line}");
+    let mut input: Vec<String> = Vec::new();
+
+    || -> Option<()> {
+        let mut lines = stdin_lines();
+        loop {
+            input.push(lines.by_ref().take(7).collect::<Vec<_>>().join(""));
+            lines.next()?;
+        }
+    }();
+
+    let mut p1 = 0;
+
+    for i in 0..input.len() {
+        for j in (i + 1)..input.len() {
+            if input[i]
+                .chars()
+                .zip(input[j].chars())
+                .all(|(a, b)| a == '.' || b == '.')
+            {
+                p1 += 1;
+            }
+        }
     }
+
+    println!("{p1}");
 }
