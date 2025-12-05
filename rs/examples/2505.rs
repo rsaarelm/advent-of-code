3b1189940912b9b47a0f879045067985f63d70d2
Risto Saarelma <risto.saarelma@iki.fi>
2025-12-05 15:03:07 +0200

    Simplify

--- rs/examples/2505.rs
+++ rs/examples/2505.rs
@@ -14,6 +14,20 @@ fn main() {
         }
     }
 
+    // Merge overlapping ranges.
+    fresh.sort_by(|a, b| a.start().cmp(b.start()));
+    let mut i = 0;
+    while i + 1 < fresh.len() {
+        if fresh[i + 1].start() <= fresh[i].end() {
+            fresh[i + 1] =
+                *fresh[i].start()..=*fresh[i + 1].end().max(fresh[i].end());
+            fresh.remove(i);
+        } else {
+            i += 1;
+        }
+    }
+
+    // P1
     println!(
         "{}",
         items
@@ -22,58 +36,9 @@ fn main() {
             .count()
     );
 
-    let mut intervals = Vec::new();
-    for i in &fresh {
-        insert(&mut intervals, i)
-    }
-
+    // P2
     println!(
         "{}",
-        intervals
-            .iter()
-            .map(|a| a.end() - a.start() + 1)
-            .sum::<i64>()
+        fresh.iter().map(|a| a.end() - a.start() + 1).sum::<i64>()
     );
-}
-
-/// Construct an ordered list of disjoint intervals.
-fn insert(
-    intervals: &mut Vec<RangeInclusive<i64>>,
-    item: &RangeInclusive<i64>,
-) {
-    for i in 0..intervals.len() {
-        if item.end() < intervals[i].start() {
-            intervals.insert(i, item.clone());
-            return;
-        }
-        if let Some(merged) = merge(&intervals[i], &item) {
-            intervals.remove(i);
-            return insert(intervals, &merged);
-        }
-        assert!(item.start() > intervals[i].start());
-    }
-
-    assert!(
-        intervals.is_empty()
-            || intervals[intervals.len() - 1].end() < item.start()
-    );
-    intervals.push(item.clone());
-}
-
-fn merge(
-    a: &RangeInclusive<i64>,
-    b: &RangeInclusive<i64>,
-) -> Option<RangeInclusive<i64>> {
-    // Ensure a.start() <= b.start().
-    let (a, b) = if b.start() < a.start() {
-        (b, a)
-    } else {
-        (a, b)
-    };
-
-    if b.start() <= a.end() {
-        Some(*a.start().min(b.start())..=*a.end().max(b.end()))
-    } else {
-        None
-    }
 }
