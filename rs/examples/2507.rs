8669917497f249d6c56deed1f4ccbb4adeddc73f
Risto Saarelma <risto.saarelma@iki.fi>
2025-12-07 08:52:12 +0200

    Day 2025.07 (rs)

--- rs/examples/2507.rs
+++ rs/examples/2507.rs
@@ -1,7 +1,59 @@
 use aoc::prelude::*;
+use memoize::memoize;
 
 fn main() {
-    for line in stdin_lines() {
-        eprintln!("{line}");
+    let mut origin: IVec2 = Default::default();
+    // Using a Vec instead of a set so memoizer can eat it later.
+    let mut splitters: Vec<IVec2> = Default::default();
+    let mut bounds = Rect::default();
+
+    for (pos, c) in stdin_grid_iter(&mut bounds) {
+        match c {
+            'S' => {
+                origin = pos.into();
+            }
+            '^' => {
+                splitters.push(pos.into());
+            }
+            _ => {}
+        }
     }
+
+    let mut beams = HashSet::from_iter([origin.x]);
+
+    let mut splits = 0;
+    for y in origin.y..bounds.height() {
+        let mut new_beams = beams.clone();
+        for &x in &beams {
+            if splitters.contains(&ivec2(x, y)) {
+                splits += 1;
+                new_beams.remove(&x);
+                new_beams.insert(x - 1);
+                new_beams.insert(x + 1);
+            }
+        }
+
+        beams = new_beams;
+    }
+
+    println!("{splits}");
+
+    println!(
+        "{}",
+        num_paths(bounds, splitters.clone(), origin)
+    );
+}
+
+#[memoize]
+fn num_paths(bounds: Rect<i32>, splitters: Vec<IVec2>, pos: IVec2) -> usize {
+    for y in pos.y..bounds.height() {
+        let p = ivec2(pos.x, y);
+        if splitters.contains(&p) {
+            return num_paths(bounds, splitters.clone(), p - ivec2(1, 0))
+                + num_paths(bounds, splitters.clone(), p + ivec2(1, 0));
+        }
+    }
+
+    // No splits, just one path.
+    1
 }
